877914c549af4333a08bc65f1418efe3ee2da43d
huhongyun <xiaomiwujiecao@gmail.com>
2018-12-23 21:13:36 +0800

    inherits

---
+++ 10. function template inherit/inherit.js
@@ -0,0 +1,27 @@
+var  util = require('util')
+// console.log(util)
+function Pet() {
+  this.name = 'unkown'
+  this.type='animal'
+}
+
+Pet.prototype.summary = function () {
+  console.log(`${this.name} is a/ an ${this.type}`)
+}
+
+Pet.prototype.setName = function (name) {
+  this.name = name
+}
+
+function Dog() {
+  Pet.call(this)
+  this.type = 'Dog'
+
+}
+
+
+util.inherits(Dog,Pet)
+
+var dog = new Dog()
+dog.setName('jj')
+dog.summary()
